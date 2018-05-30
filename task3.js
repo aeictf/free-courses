b1668a4646c846bf94128cf7e184c9117913c649
aeictf <30719305+aeictf@users.noreply.github.com>
2018-05-30 23:58:04 +0300

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,31 @@
+function Sep(){
+  var l=process.argv.length-2;
+var n=process.argv[2];
+  var i=2;
+  while(l>1){
+    i++;
+  var str=process.argv[i];
+  n=n+str;
+  l--;
+}
+  var a=n.match(/[aeiou]/ig);
+  var b=n.match(/[bcdfghjklmnpqrstvwxyz]/ig);
+  var c=n.match(/[0123456789]/ig);
+  if(a!==null){
+  for (var i=0;i<a.length;i++)
+    process.stdout.write(a[i]);
+  }
+  if(b!==null){
+  process.stdout.write(' ');
+  for(var i=0;i<b.length;i++)
+    process.stdout.write(b[i]);
+  }
+  if(c!==null){
+  process.stdout.write(' ');
+  for(var i=0;i<c.length;i++)
+    process.stdout.write(c[i]);
+  }
+
+}
+
+Sep();
