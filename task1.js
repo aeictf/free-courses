364ad8aa5b4ade6997ed13bcb358f0fa4561bcc3
aeictf <30719305+aeictf@users.noreply.github.com>
2018-05-30 23:04:56 +0300

    Update task1.js

--- task1.js
+++ task1.js
@@ -2,11 +2,11 @@ function isPalindrom(){
   var str=process.argv[2];
   s=str.toLowerCase().replace(/[^a-zA-Zа-яА-Я]/g, '');
   var length=s.length;
-  for(var i=0;i<length/2;i+=1)
+  for(var i=0;i<length/2;i++)
     if (s.charAt(i) !== s.charAt(length-(1+i))) {
-         process.stdout.write('no');
+         process.stdout.write('NO');
          return false;
       }
-   process.stdout.write('yes');
+   process.stdout.write('YES');
 }
 isPalindrom();
