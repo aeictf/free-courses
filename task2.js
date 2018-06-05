809e513b344838c0a4549947a5bd06a9a3a5734e
aeictf <30719305+aeictf@users.noreply.github.com>
2018-06-05 12:32:14 +0300

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,11 +1,17 @@
- function Fib(){
 var n=process.argv[2];
+ function Fib(n){
 var f2=1;
 var f1=0;
-for (var i=0;i<n;i++){
-  f2+=f1;
-  f1=f2-f1;
-}
+if (n>=0)
+  for (var i=0;i<n;i++){
+    f2+=f1;
+    f1=f2-f1;
+  }
+else
+    for(var i=n;i<0;i++){
+      f1=f2-f1;
+      f2-=f1;
+    }
 console.log(f1);
 }
-Fib();
+Fib(n);
