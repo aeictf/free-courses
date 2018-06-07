588147322ad9cfec7bbffdb3a3f693e498f4dcdd
aeictf <30719305+aeictf@users.noreply.github.com>
2018-06-07 09:22:01 +0300

    Update task3.js

--- task3.js
+++ task3.js
@@ -8,30 +8,41 @@ n=n+str;
 l--;
 }
 
+var l=process.argv.length-2;
+var n=process.argv[2];
+var i=2;
+while(l>1){
+  i++;
+var str=process.argv[i];
+n=n+str;
+l--;
+}
+
 function Sep(n){
-  var s=0;
+  var s='';
+  var l=0;
   var a=n.match(/[aeiou]/ig);
   var b=n.match(/[bcdfghjklmnpqrstvwxyz]/ig);
   var c=n.match(/[0123456789]/ig);
   if(a!==null){
-    s++;
   for (var i=0;i<a.length;i++)
-    process.stdout.write(a[i]);
+  s+=a[i];
+  l++;
   }
   if(b!==null){
-    if(s>0)
-      process.stdout.write(' ');
+    if(l>0)
+    s+=' ';
   for(var i=0;i<b.length;i++)
-    process.stdout.write(b[i]);
-  s++;
+  s+=b[i];
+  l++;
   }
   if(c!==null){
-    if(s>0)
-      process.stdout.write(' ');
+    if(l>0)
+    s+=' ';
   for(var i=0;i<c.length;i++)
-    process.stdout.write(c[i]);
+    s+=c[i];
   }
-
+process.stdout.write(s);
 }
 
 Sep(n);
