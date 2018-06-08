e678b7b81ec4c2fee3b9a572f148e23dc8878a48
aeictf <30719305+aeictf@users.noreply.github.com>
2018-06-08 14:24:28 +0300

    Update task3.js

--- task3.js
+++ task3.js
@@ -8,37 +8,23 @@ n=n+str;
 l--;
 }
 
-var l=process.argv.length-2;
-var n=process.argv[2];
-var i=2;
-while(l>1){
-  i++;
-var str=process.argv[i];
-n=n+str;
-l--;
-}
-
 function Sep(n){
   var s='';
-  var l=0;
-  var a=n.match(/[aeiou]/ig);
-  var b=n.match(/[bcdfghjklmnpqrstvwxyz]/ig);
+  var a=n.match(/[aeiouy]/ig);
+  var b=n.match(/[bcdfghjklmnpqrstvwxz]/ig);
   var c=n.match(/[0123456789]/ig);
   if(a!==null){
   for (var i=0;i<a.length;i++)
   s+=a[i];
-  l++;
+  s+=' ';
   }
+
   if(b!==null){
-    if(l>0)
-    s+=' ';
   for(var i=0;i<b.length;i++)
   s+=b[i];
-  l++;
   }
+  s+=' ';
   if(c!==null){
-    if(l>0)
-    s+=' ';
   for(var i=0;i<c.length;i++)
     s+=c[i];
   }
